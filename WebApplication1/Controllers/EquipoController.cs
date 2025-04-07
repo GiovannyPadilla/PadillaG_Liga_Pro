ac6a657569528028c038f270993e9f2b69cce3f3
GiovannyPadilla <giovanny.padilla@udla.edu.ec>
2025-04-07 09:06:11 -0500

    Creacion de la lista de equipos y creación de repositorios

---
+++ WebApplication1/Controllers/EquipoController.cs
@@ -0,0 +1,29 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PadillaG_Liga_Pro.Models;
+using PadillaG_Liga_Pro.Repositories;
+
+namespace PadillaG_Liga_Pro.Controllers
+{
+    public class EquipoController : Controller
+    {
+
+        public ActionResult View()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            EquipoRepository equipoRepository = new EquipoRepository();
+            var equipos = equipoRepository.DevuelveListadoEquipos();
+            
+            equipos = equipos.OrderByDescending(item => item.partidosGanados);
+            //equipos = equipos.Where(item => item.Nombre == "Liga de Quito");
+
+            return View(equipos);
+        }
+
+    }
+}
